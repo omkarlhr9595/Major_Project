d6c45b4777f2cc331ad16bafdd14a1d89e473e21
Omkar Lohar <omkarlohar91@gmail.com>
2022-03-22 16:17:45 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="w-[100%] h-[100vh] bg-[url('/img/bg1.svg')] grid place-items-center bg-no-repeat bg-center bg-cover">
+        <h1 class="text-white font-bold text-6xl font-mono">Result Analyzer</h1>
+    </div>
+    <div class="w-[100%] h-[100vh]">
+        <div class="w-[500px] h-[500px]">
+
+        </div>
+    </div>
+</body>
+
+</html>
